a2113f27ffae93b2de52d07febd20c59b547c143
Domore <placebo9132@gmail.com>
2025-03-10 21:06:59 +0500

    build

--- src/components/pages/HomePage/fragments/AnimateSection/AnimateSection.tsx
+++ src/components/pages/HomePage/fragments/AnimateSection/AnimateSection.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import s from './AnimateSection.module.less'
-import { Canvas } from '@react-three/fiber'
 import { Words } from './fragments'
 
 const AnimateSection: FC = () => {
